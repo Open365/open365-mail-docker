d293c5a3022ee40ffe73910446a1a42e042ea3d4
Vishesh Handa <vishesh.handa@eyeos.com>
2016-05-27 15:26:11 +0200

    Fix buggy crudini
    crudini puts extra spaces around an '=', which is not something breeze
    likes.

--- scripts/updateKontactAnimationSettings.sh
+++ scripts/updateKontactAnimationSettings.sh
@@ -16,3 +16,7 @@ crudini --set $KDECONF/kmail2rc Reader htmlMail true
 crudini --set $KDECONF/kmail2rc MainFolderView ToolTipDisplayPolicy 2
 crudini --set $KDECONF/kmail2rc MessageListView MessageToolTipEnabled false
 crudini --set $KDECONF/kmail2rc TemplateParser replySameFormat true
+
+sed -i 's$ = $=$' $KDECONF/breezerc
+sed -i 's$ = $=$' $KDECONF/kdeglobals
+sed -i 's$ = $=$' $KDECONF/kmail2rc
